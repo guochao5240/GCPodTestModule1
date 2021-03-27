9651a9d2ef4362e2b580b0d4b7c622f653b04157
GuoChao <524086708@qq.com>
2021-03-27 16:20:26 +0800

    1

---
+++ GCPodTestModule1/Classes/GCtest1VC.h
@@ -0,0 +1,17 @@
+//
+//  GCtest1VC.h
+//  GCPodTestModule1_Example
+//
+//  Created by guochao on 2021/3/27.
+//  Copyright © 2021 524086708@qq.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface GCtest1VC : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
