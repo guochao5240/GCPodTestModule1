d7dcdbc5723b09e1e9aa9510abad14f110a7640c
GuoChao <524086708@qq.com>
2021-03-27 16:20:12 +0800

    update

---
+++ Example/GCPodTestModule1/GCtest1VC.m
@@ -0,0 +1,32 @@
+//
+//  GCtest1VC.m
+//  GCPodTestModule1_Example
+//
+//  Created by guochao on 2021/3/27.
+//  Copyright © 2021 524086708@qq.com. All rights reserved.
+//
+
+#import "GCtest1VC.h"
+
+@interface GCtest1VC ()
+
+@end
+
+@implementation GCtest1VC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
